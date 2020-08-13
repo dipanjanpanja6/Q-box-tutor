9d47068cc7cbf93526e848c6ea4eedf1ca9ecf25
ishanajmeri <ajmeriishansurat@gmail.com>
2020-08-13 23:47:31 +0530

    editor

--- src/Components/edit/Editor.js
+++ src/Components/edit/Editor.js
@@ -10,14 +10,43 @@ const EditorJS = (props) => {
     }
     // console.log(props.data);
     return (
-        <Editor 
+        <Editor
             wrapperClassName="demo-wrapper"
             editorClassName="demo-editor"
             placeholder={props.placeholder}
             onContentStateChange={onEditorStateChange}
+            toolbar={{
+                inline: { inDropdown: true },
+                list: { inDropdown: true },
+                textAlign: { inDropdown: true },
+                link: { inDropdown: true },
+                history: { inDropdown: true },
+                image: { uploadCallback: uploadImageCallBack, alt: { present: true, mandatory: true } },
+            }}
         />
 
     )
 }
 export default EditorJS
- 
+
+function uploadImageCallBack(file) {
+    return new Promise(
+        (resolve, reject) => {
+            const xhr = new XMLHttpRequest();
+            xhr.open('POST', 'https://api.imgur.com/3/image');
+            xhr.setRequestHeader('Authorization', 'Client-ID 8d26ccd12712fca');
+            const data = new FormData();
+            data.append('image', file);
+            xhr.send(data);
+            xhr.addEventListener('load', () => {
+                const response = JSON.parse(xhr.responseText);
+                resolve(response);
+            });
+            xhr.addEventListener('error', () => {
+                const error = JSON.parse(xhr.responseText);
+                reject(error);
+            });
+        }
+    );
+}
+
